ab227321d5196eed5331b3c79377bde241b00236
abeedkoul <koulabeed@gmail.com>
2023-06-28 11:59:11 +0530

    Made some plans for changes in the UI and functionality

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,7 +1,7 @@
 
 
 import styles from './Example.module.css'
-import { useState,React } from 'react';
+import { useState, React } from 'react';
 
 const Example = () => {
 
@@ -9,16 +9,16 @@ const Example = () => {
 
     function lol(e) {
         e.preventDefault();
-        setchar(e.target.value.split(/\s+/).length-1);
-        
-
-      
-        
+        setchar(e.target.value.split(/\s+/).length - 1);
 
 
 
-        
-       //console.log(entireInput.split(' ').length)
+
+
+
+
+
+        //console.log(entireInput.split(' ').length)
     }
 
 
@@ -28,23 +28,25 @@ const Example = () => {
             <div className={styles.card}>
                 <h2>Responsive paragraph word counter</h2>
                 <div>
-                    <input onChange={(e)=> {lol(e)}} type='text' className={styles.inputBox}>
+                    <input onChange={(e) => { lol(e) }} type='text' className={styles.inputBox}>
                     </input>
                 </div>
                 <div className={styles.info}>
                     <div>Word Count = {char}</div>
-                 
-                </div>
 
+                </div>
+                <p>This is a word counter on which I am currently making some updates</p>
+                <p>This Works and functions properly but still can be made much better and is an good opputurnity to practise JS and React</p>
                 
-                
-                
+
+
+
             </div>
 
 
 
         </div>
-        
+
     )
 }
 
